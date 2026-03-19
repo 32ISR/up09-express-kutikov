4a2cb7076196003d0f26b84247fb7a77342bd2f5
Pasha <39154112+ktkv419@users.noreply.github.com>
2026-03-19 04:32:22 +0000

    123

--- marketplace/index.js
+++ marketplace/index.js
@@ -2,53 +2,76 @@ const express = require("express")
 const db = require("./db")
 const bcr = require("bcryptjs")
 const jwt = require("jsonwebtoken")
+const cors = require("cors")
+
 const app = express()
+app.use(cors())
+app.use(express.json())
+
+const SECRET = "asdasdasdasdzasd"
 
 const PORT = 3000
 
-app.get("/", (req,res) => {
-    return res.status(200).json({text: "hello world"})
+const auth = (req, res, next) => {
+    const authHeader = req.headers.authorization
+    if (!authHeader) return res.status(401).json({error: "No token provided"})
+
+    const token = authHeader.split(" ")[1] // ["Bearer", "eyDCjsakdj"]
+    if (!token) return res.status(401).json({error: "Invalid token form"})
+
+    try {
+        const decoded = jwt.verify(token, SECRET)
+        req.user = decoded
+        next()
+    } catch (error) {
+        return res.status(403).json({error: "Invalid or expired token"})
+    }
+}
+
+app.get("/", (req, res) => {
+    return res.status(200).json({ text: "hello world" })
 })
 
 app.post("/auth/signin", (req, res) => {
-    
+
 })
 app.post("/auth/signup", (req, res) => {
     try {
-        const {username, password, email} = req.body
-    
+        const { username, password, email } = req.body
+
         if (!username || !password) {
-            return res.status(400).json({error: "."})
+            return res.status(400).json({ error: "." })
         }
 
         if (username.length < 3) {
-            return res.status(400).json({error: "Недостаточно символов в пароле"})
+            return res.status(400).json({ error: "Недостаточно символов в пароле" })
         }
         if (password.length < 6) {
-            return res.status(400).json({error: "Недостаточно символов в пароле"})
+            return res.status(400).json({ error: "Недостаточно символов в пароле" })
         }
 
         const existing = db.prepare(
             "SELECT id FROM users WHERE username = ?"
         ).get(username)
 
-        if (existing) return res.status(409).json({error: "Пользователь уже существует"})
-    
+        if (existing) return res.status(409).json({ error: "Пользователь уже существует" })
+
         const salt = bcr.genSaltSync(10)
         const hash = bcr.hashSync(password, salt)
         const role = "user"
 
         const info = db.prepare(`INSERT INTO users (username, email, password, role)
             VALUES(?,?,?,?)`).run(username.trim(), email.trim(), hash, role)
-        
+
         const newUser = db.prepare(`SELECT * FROM users WHERE id = ?`).get(info.lastInsertRowid)
 
-        const {password: _, ...safeUser} = newUser
+        const { password: _, ...safeUser } = newUser
 
-        const token = jwt.sign({...safeUser}, SECRET, {expiresIn: "24h"})
-        res.status(201).json({success: true, token, user: safeUser})
-    } catch(err) {
-
+        const token = jwt.sign({ ...safeUser }, SECRET, { expiresIn: "24h" })
+        res.status(201).json({ success: true, token, user: safeUser })
+    } catch (err) {
+        console.error(err)
+        return res.status(500).json({ error: "Server failed" })
     }
 })
 
@@ -61,37 +84,37 @@ app.get("/api/items", (req, res) => {
         return res.status(200).json(items)
     } catch (err) {
         console.error(err)
-        return res.status(500).json({error: "Failed to fetch"})
+        return res.status(500).json({ error: "Failed to fetch" })
     }
 })
 
-app.post("/api/items", (req, res) => {
+app.post("/api/items", auth, (req, res) => {
     console.log(req.body)
     try {
-        const {title, description, price, imageUrl}= req.body
+        const { title, description, price, imageUrl } = req.body
 
         if (!title || !title.trim()) {
             return res
                 .status(400)
-                .json({error: "Нужно название"})
+                .json({ error: "Нужно название" })
         }
 
         if (!description || !description.trim()) {
             return res
                 .status(400)
-                .json({error: "Нужно описание"})
+                .json({ error: "Нужно описание" })
         }
 
         if (!price || price <= 0) {
             return res
                 .status(400)
-                .json({error: "Нужна цена"})
+                .json({ error: "Нужна цена" })
         }
 
         const info = db.prepare(`
             INSERT INTO items (title, description, price, imageUrl, userId, username, status, highestBid, bidCount)
-            VALUES (?, ?, ?, ?, ?, ?, "active", NULL, 0)
-            `).run(title.trim(), description.trim(), 
+            VALUES (?, ?, ?, ?, ?, ?, 'active', NULL, 0)
+            `).run(title.trim(), description.trim(),
             parseFloat(price), imageUrl || null,
             req.user.id, req.user.username)
 
@@ -102,7 +125,7 @@ app.post("/api/items", (req, res) => {
         return res.status(201).json(newItem)
     } catch (err) {
         console.error(err)
-        return res.status(500).json({error: "Failed to create"})
+        return res.status(500).json({ error: "Failed to create" })
     }
 })
 
